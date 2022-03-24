a5791f8e871b54ec48ee11ee60e1f0ca9a2dfea3
ryu-sugi <ryuu085@gmail.com>
2022-03-24 14:26:39 +0900

    チェックボックスによる表示切り替え実装

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ Vue.filter('number_format', function(val){
 var app = new Vue ({
   el: '#app',
   data: {
-    // 表示中の商品数
-    count: 0,
     // 「セール対象」のチェック状態(true:チェック有り、false:チェック無し)
     showSaleItem: false,
     // 「送料無料」のチェック状態(true:チェック有り、false:チェック無し)
@@ -20,8 +18,46 @@ var app = new Vue ({
       {name: 'Raphael <br> スマホケース', price: 1580, image: 'images/02.jpg', delv: 0, isSale: true},
       {name: 'Gabriel <br> スマホケース', price: 1580, image: 'images/03.jpg', delv: 240, isSale: true},
       {name: 'Uriel <br> スマホケース', price: 980, image: 'images/04.jpg', delv: 0, isSale: true},
-      {name: 'Ariel <br> スマホケース', price: 980, image: 'images/05.jpg', delv: 0, isSale: true},
-      {name: 'Azrael <br> スマホケース', price: 1580, image: 'images/06.jpg', delv: 0, isSale: true}
+      {name: 'Ariel <br> スマホケース', price: 980, image: 'images/05.jpg', delv: 0, isSale: false},
+      {name: 'Azrael <br> スマホケース', price: 1580, image: 'images/06.jpg', delv: 0, isSale: false}
     ]
+  }, // ← オプションを区切る「,」を忘れないようにする。
+  watch: {
+    // 「セール対象」チェックボックスの状態を監視するウォッチャ
+    showSaleItem: function(newval, oldVal) {
+      // ここでproductsの配列を書き換える
+      console.log('showSaleItemウォッチャが呼び出されました。');
+    },
+    // 「送料無料」チェックボックスの状態を監視するウォッチャ
+    showDelvFree: function(newval, oldVal) {
+      // ここでproductsの配列を書き換える
+      console.log('showDelvFreeウォッチャが呼び出されました。');
+    }
+  },
+  computed: {
+    // 絞り込み後の商品リストを返す算出プロパティ
+    filteredList: function() {
+      // 絞り込み後の商品リストを格納する新しい配列
+      var newList = [];
+      for (var i=0; i<this.products.length; i++) {
+        // 表示対象かどうかを判定するフラグ
+        var isShow = true;
+        // i番目の商品が表示対象かどうかを判定する
+        if (this.showSaleItem && !this.products[i].isSale) {
+          // 「セール対象」チェック有りで、セール対象商品ではない場合
+          isShow = false; // この商品は表示しない
+        }
+        if (this.showDelvFree && this.products[i].delv > 0) {
+          // 「送料無料」チェック有りで、送料有りの商品の場合
+          isShow = false; // この商品は表示しない
+        }
+      // 表示対象の商品だけwを新しい配列に追加する
+      if (isShow) {
+        newList.push(this.products[i]);
+      }
+    }
+      // 絞り込み後の商品リストを返す
+      return newList;
+    }
   }
 });
